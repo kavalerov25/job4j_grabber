2d09f37d28d55b1a30c7250c93327b31b84bc695
kavalerov25 <paradise_777@mail.ru>
2022-04-02 18:16:34 +0300

    2.3. Загрузка деталей поста. [#285212]

--- job4j_grabber/src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ job4j_grabber/src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -7,16 +7,15 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import ru.job4j.grabber.utils.HarbCareerDateTimeParser;
 
+import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 public class HabrCareerParse {
-
     private static final String SOURCE_LINK = "https://career.habr.com";
-
     private static final String PAGE_LINK = String.format("%s/vacancies/java_developer", SOURCE_LINK);
-
     private static final String PAGE_FIVE = "%s/vacancies/java_developer?PAGE=%s";
+
 
     public static void main(String[] args) throws Exception {
         for (int page = 1; page <= 5; page++) {
@@ -38,7 +37,23 @@ public class HabrCareerParse {
                 String vacancyName = titleElement.text();
                 String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
                 System.out.printf("%s: %s %s%n", new HarbCareerDateTimeParser().parse(dateElement.attr("datetime")), vacancyName, link);
+
+                String text = null;
+                try {
+                    text = retrieveDescription(link);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("< " + text + " />");
             });
         }
+    }
+
+    private static String retrieveDescription(String link) throws IOException {
+        Connection connection = Jsoup.connect(link);
+        Document document = connection.get();
+        Elements rows = document.select(".job_show_description__body");
+        Elements descriptionElement = rows.select(".job_show_description__vacancy_description");
+        return descriptionElement.text();
     }
 }
